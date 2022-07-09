93c6cad7b116d9c10329c9583567581ab365a919
powerinv <cambridge_james@foxmail.com>
2022-07-10 04:17:58 +0800

    修复项目打包后无法正常引用的问题

---
+++ build/rollup.config.js
@@ -0,0 +1,49 @@
+'use strict'
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import path from 'path';
+import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
+import pkg from '../package.json';
+
+const deps = Object.keys(pkg.dependencies);
+const vue = require('rollup-plugin-vue');
+
+export default [
+    {
+        input: path.resolve(__dirname, '../packages/index.ts'),
+        output: [
+            {
+                format: 'es',
+                file: pkg.module,
+            }
+        ],
+        plugins: [
+            terser(),
+            nodeResolve(),
+            vue({
+                target: 'browser',
+                css: false,
+                exposeFilename: false,
+            }),
+            typescript({
+                tsconfigOverride: {
+                    compilerOptions: {
+                        declaration: true,
+                    },
+                    'include': [
+                        'packages/**/*',
+                        'typings/shims-vue.d.ts',
+                    ],
+                    'exclude': [
+                        'node_modules',
+                        'packages/**/__tests__/*',
+                    ],
+                },
+                abortOnError: false,
+            }),
+        ],
+        external(id) {
+            return /^vue/.test(id) || deps.some(k => new RegExp('^' + k).test(id));
+        },
+    },
+]
--- packages/NePanel/src/NePanel.vue
+++ packages/NePanel/src/NePanel.vue
@@ -6,7 +6,7 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: "NePanel",
+  name: "ne-panel",
 })
 </script>
 
