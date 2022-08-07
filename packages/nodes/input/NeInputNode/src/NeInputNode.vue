15f473d7577c0f1fba75440cf2e3eece84aafd1a
powerinv <cambridge_james@foxmail.com>
2022-08-07 19:10:14 +0800

    添加节点Resize事件

--- packages/nodes/input/NeInputNode/src/NeInputNode.vue
+++ packages/nodes/input/NeInputNode/src/NeInputNode.vue
@@ -1,46 +1,24 @@
 <template>
-  <g ref="ne-text" class="ne-node ne-text" :style="`--width: ${nodePanelConf.width}px; --height: ${nodePanelConf.height}px;`"
-     :transform="`translate(${nodePanelConf.x},${nodePanelConf.y})`">
-    <rect class="background" :transform="`translate(-0.5, -0.5)`"></rect>
+  <g ref="nodePanel" class="ne-node ne-text" :style="`--width: ${nodePanelConf.width}px; --height: ${nodePanelConf.height}px;`"
+     :transform="`translate(${nodePanelConf.x},${nodePanelConf.y})`"
+     @mousedown.left.stop.prevent>
+    <rect class="background" :x="-0.5" :y="-0.5"></rect>
     <g ref="input-title-group" class="title-group">
       <rect ref="text-title-back" class="title-back text-title-back"></rect>
       <text ref="text-title-text" class="title-text" x="4" y="14">{{ nodePanelConf.title }}</text>
       <circle ref="output-point" class="output-point" :cx="nodePanelConf.width - 6" cy="10" r="4"></circle>
     </g>
+    <g ref="input-footer-group" class="footer-group" :transform="`translate(0,${nodePanelConf.height - 20})`">
+      <rect class="resize" :x="nodePanelConf.width - 10" :y="10" :width="10" :height="10"
+            @mousedown.left.stop.prevent="MouseEventProcessor.onResizeDown"></rect>
+      <ne-comp-svg type="resize" class="resize" :x="nodePanelConf.width - 20" :width="20" :height="20"></ne-comp-svg>
+    </g>
   </g>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
-
-export default defineComponent({
-  name: "NeInputNode",
-  components: {},
-  props: {
-    x: {
-      type: Number,
-      default: 0,
-      required: true
-    },
-    y: {
-      type: Number,
-      default: 0,
-      required: true
-    }
-  },
-  setup(propsData) {
-    const nodePanelConf = ref({
-      x: propsData.x,
-      y: propsData.y,
-      width: 120,
-      height: 40,
-      title: "输入"
-    });
-    return {
-      nodePanelConf
-    };
-  }
-});
+import NeInputNodeIndex from "./js/index";
+export default NeInputNodeIndex;
 </script>
 
 <style lang="scss" scoped>
---
+++ packages/nodes/input/NeInputNode/src/js/event/mouseEventProcessor.ts
@@ -0,0 +1,56 @@
+import { NeInputPanelIntf } from "@/nodes/input/NeInputNode/src/js/interface/neInputPanelIntf";
+import { Ref } from "vue";
+import { Point } from "@/js/interface/2d/Point";
+
+/**
+ * 根据节点获取整个面板
+ *
+ * @param nodeElement
+ */
+const getPanelElement = (nodeElement: HTMLElement | undefined): HTMLElement | undefined | null => {
+  let parentElement = nodeElement as HTMLElement | undefined | null;
+  while (parentElement !== undefined && parentElement !== null && parentElement.tagName !== "svg") {
+    parentElement = parentElement.parentElement;
+  }
+  return parentElement;
+};
+
+export const getMouseEventProcessor = (nodePanel: Ref<HTMLElement | undefined>, nodePanelConf: Ref<NeInputPanelIntf>) => {
+  /**
+   * 节点大小改变事件响应方法
+   *
+   * @param event 鼠标事件
+   */
+  const onResizeDown = (event: MouseEvent): void => {
+    const panelElement = getPanelElement(nodePanel.value);
+    const starting: Point = {
+      x: event.clientX,
+      y: event.clientY
+    };
+    const resizeFunc = (event: Event): void => {
+      if (event instanceof MouseEvent) {
+        const mouseEvent = event as MouseEvent;
+        if (nodePanelConf.value.width + mouseEvent.clientX - starting.x >= nodePanelConf.value.minWidth) {
+          nodePanelConf.value.width += mouseEvent.clientX - starting.x;
+          starting.x = mouseEvent.clientX;
+        }
+        if (nodePanelConf.value.height + mouseEvent.clientY - starting.y >= nodePanelConf.value.minHeight) {
+          nodePanelConf.value.height += mouseEvent.clientY - starting.y;
+          starting.y = mouseEvent.clientY;
+        }
+      }
+    };
+    const mouseUpFunc = (event: Event): void => {
+      if (event instanceof MouseEvent) {
+        panelElement?.removeEventListener("mousemove", resizeFunc);
+        panelElement?.removeEventListener("mouseup", mouseUpFunc);
+      }
+    };
+    panelElement?.addEventListener("mousemove", resizeFunc);
+    panelElement?.addEventListener("mouseup", mouseUpFunc);
+  };
+
+  return {
+    onResizeDown
+  };
+};
