691df346230ed81db86b19e59b58ac86939c2fae
powerinv <cambridge_james@foxmail.com>
2022-08-26 03:10:00 +0800

    修改覆盖率文件目录，忽略coverage目录提交

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript",
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/packages/$1"
-  }
+  },
+  coverageDirectory: "tests/coverage"
 };
